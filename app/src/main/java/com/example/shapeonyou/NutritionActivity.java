243c02b3b4a7c7d1f8bb21316ad181397cccca82
douaa <douaa.BENHILA@etu.univ-amu.fr>
2025-06-01 20:31:10 +0200

    ajouter un bouton de retour (a la page de dashboard.xml ) dans ma page nutrition.xml

--- app/src/main/java/com/example/shapeonyou/NutritionActivity.java
+++ app/src/main/java/com/example/shapeonyou/NutritionActivity.java
@@ -1,6 +1,8 @@
 package com.example.shapeonyou;
 
 import android.os.Bundle;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class NutritionActivity extends AppCompatActivity {
@@ -8,5 +10,19 @@ public class NutritionActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.nutrition);
+
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+
+
+        Button retour = findViewById(R.id.btnRetour);
+        retour.setOnClickListener(v -> finish());
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        finish();
+        return true;
     }
 }
