db4d3bc1ee11b825a634030621388b6b862172eb
numa <numa.efremon@etu.univ-amu.fr>
2025-06-15 23:14:45 +0200

    feature : add => page de présentation estétique

---
+++ app/src/main/java/com/example/shapeonyou/WelcomeActivity.java
@@ -0,0 +1,34 @@
+package com.example.shapeonyou;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    private Button btnStart;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+
+        // 1) bind view
+        btnStart = findViewById(R.id.btnStart);
+
+        // 2) clic sur "C'est parti !" → lancement de Quest1Activity
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(WelcomeActivity.this, LoginActivity.class);
+                // on nettoie la stack si besoin
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+}
